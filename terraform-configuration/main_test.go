ae7e856a097c426b22a791ccc6685a9907b927be
ariel.domchik <ariyonzor@gmail.com>
2023-04-23 12:56:16 +0300

    test new go configuration

--- terraform-configuration/main_test.go
+++ terraform-configuration/main_test.go
@@ -1,52 +1,34 @@
 package test
 
 import (
-    "fmt"
-    "os"
-    "path/filepath"
-    "testing"
-
-    "github.com/gruntwork-io/terratest/modules/aws"
-    "github.com/gruntwork-io/terratest/modules/random"
-    "github.com/gruntwork-io/terratest/modules/terraform"
-    "github.com/stretchr/testify/assert"
+	"testing"
+	"github.com/gruntwork-io/terratest/modules/aws"
+	"github.com/gruntwork-io/terratest/modules/random"
+	"github.com/gruntwork-io/terratest/modules/terraform"
+	"github.com/stretchr/testify/assert"
 )
 
-func TestTerraform(t *testing.T) {
-    terraformOptions := &terraform.Options{
-        TerraformDir: "../",
-        Vars: map[string]interface{}{
-            "region":           "us-east-1",
-            "suffix":           random.UniqueId(),
-            "instance_type":    "t3.small",
-        },
-    }
+func TestEksClusterCreation(t *testing.T) {
+	terraformOptions := &terraform.Options{
+		TerraformDir: "../terraform-eks",
+		Vars: map[string]interface{}{
+			"region": "us-west-2",
+		},
+	}
+	defer terraform.Destroy(t, terraformOptions)
 
-    // Clean up everything at the end of the test
-    defer terraform.Destroy(t, terraformOptions)
+	terraform.InitAndApply(t, terraformOptions)
 
-    // Deploy the infrastructure
-    terraform.InitAndApply(t, terraformOptions)
+	region := aws.GetRandomStableRegion(t, nil, nil)
+	session, err := aws.NewSessionE(t, &aws.Options{
+		Region: region,
+	})
+	assert.NoError(t, err)
 
-    // Run Terraform output to get the outputs
-    clusterEndpoint := terraform.Output(t, terraformOptions, "cluster_endpoint")
-    clusterSecurityGroup := terraform.Output(t, terraformOptions, "cluster_security_group_id")
-    region := terraform.Output(t, terraformOptions, "region")
+	clusterName := terraform.Output(t, terraformOptions, "cluster_name")
 
-    // Verify that the outputs are valid
-    assert.NotEmpty(t, clusterEndpoint)
-    assert.NotEmpty(t, clusterSecurityGroup)
-    assert.Equal(t, "us-east-1", region)
+	err = aws.EksPing(t, session, clusterName)
+	assert.NoError(t, err)
 
-    // Use the AWS SDK to make sure the cluster endpoint is reachable
-    awsRegion := aws.GetRegion(t, region)
-    awsSession, err := aws.NewSessionE(t, &aws.Options{
-        Profile: awsProfile,
-        Region:  awsRegion,
-    })
-    if err != nil {
-        t.Fatalf("Error creating AWS session: %v", err)
-    }
-    pingResult := aws.EksPing(t, awsSession, clusterEndpoint)
-    assert.True(t, pingResult)
+	// Add more assertions here to validate that the EKS cluster was created successfully.
 }

b1b191b030aef7a9f79b0b81763afa0298f107d1
ariel.domchik <ariyonzor@gmail.com>
2023-04-27 09:03:15 +0300

    Added application as helm chart

---
+++ terraform-configuration/README.txt
@@ -0,0 +1,1 @@
+REMEMBER TO CHANGE THE KEY IN .terraform/modules/eks.kms/main.tf line 250 kms.this change the key name
--- terraform-configuration/main.tf
+++ terraform-configuration/main.tf
@@ -1,100 +1,198 @@
 provider "aws" {
-  region = var.region
+  region = local.region
+}
+
+provider "kubernetes" {
+  host                   = module.eks.cluster_endpoint
+  cluster_ca_certificate = base64decode(module.eks.cluster_certificate_authority_data)
+  token                  = data.aws_eks_cluster_auth.this.token
+}
+
+provider "helm" {
+  kubernetes {
+    host                   = module.eks.cluster_endpoint
+    cluster_ca_certificate = base64decode(module.eks.cluster_certificate_authority_data)
+    token                  = data.aws_eks_cluster_auth.this.token
+  }
+}
+
+provider "bcrypt" {}
+
+data "aws_eks_cluster_auth" "this" {
+  name = module.eks.cluster_name
 }
 
 data "aws_availability_zones" "available" {}
 
 locals {
-  cluster_name = "education-eks-${random_string.suffix.result}"
-}
+  name   = basename(path.cwd)
+  region = "us-east-1"
 
-resource "random_string" "suffix" {
-  length  = 8
-  special = false
-}
-
-module "vpc" {
-  source  = "terraform-aws-modules/vpc/aws"
-  version = "3.19.0"
-
-  name = "education-vpc"
-
-  cidr = "10.0.0.0/16"
-  azs  = slice(data.aws_availability_zones.available.names, 0, 3)
-
-  private_subnets = ["10.0.1.0/24", "10.0.2.0/24", "10.0.3.0/24"]
-  public_subnets  = ["10.0.4.0/24", "10.0.5.0/24", "10.0.6.0/24"]
-
-  enable_nat_gateway   = true
-  single_nat_gateway   = true
-  enable_dns_hostnames = true
-
-  public_subnet_tags = {
-    "kubernetes.io/cluster/${local.cluster_name}" = "shared"
-    "kubernetes.io/role/elb"                      = 1
-  }
-
-  private_subnet_tags = {
-    "kubernetes.io/cluster/${local.cluster_name}" = "shared"
-    "kubernetes.io/role/internal-elb"             = 1
-  }
-}
-
-module "eks" {
-  source  = "terraform-aws-modules/eks/aws"
-  version = "19.5.1"
-
-  cluster_name    = local.cluster_name
   cluster_version = "1.25"
 
-  vpc_id                         = module.vpc.vpc_id
-  subnet_ids                     = module.vpc.private_subnets
+  vpc_cidr = "10.0.0.0/16"
+  azs      = ["us-east-1a", "us-east-1b"] #slice(data.aws_availability_zones.available.names, 0, 3) ["us-east-1a", us-east-1b", us-east-1c"]
+
+  tags = {
+    Blueprint  = local.name
+    GithubRepo = "github.com/aws-ia/terraform-aws-eks-blueprints"
+  }
+}
+
+################################################################################
+# Cluster
+################################################################################
+
+#tfsec:ignore:aws-eks-enable-control-plane-logging
+module "eks" {
+  source  = "terraform-aws-modules/eks/aws"
+  version = "~> 19.12"
+
+  cluster_name                   = local.name
+  cluster_version                = local.cluster_version
   cluster_endpoint_public_access = true
 
+  # EKS Addons
   cluster_addons = {
-    coredns = {
-        most_recent = true
-    }
-    kube-proxy = {
-        most_recent = true
-    }
-    vpc-cni = { 
-        most_recent = true
-    }
+    coredns    = {}
+    kube-proxy = {}
+    vpc-cni    = {}
   }
-  eks_managed_node_group_defaults = {
-    ami_type = "AL2_x86_64"
 
-  }
+  vpc_id     = module.vpc.vpc_id
+  subnet_ids = module.vpc.private_subnets
 
   eks_managed_node_groups = {
     initial = {
-      name = "node-group"
-
-      instance_types = ["t3.small"]
-
+      instance_types = ["t3.medium"]
       min_size     = 1
-      max_size     = 3
-      desired_size = 2
+      max_size     = 4
+      desired_size = 3
     }
   }
+
+  tags = local.tags
 }
 
-module "karpenter" {
-  source = "terraform-aws-modules/eks/aws//modules/karpenter"
+################################################################################
+# Kubernetes Addons
+################################################################################
 
-  cluster_name = module.eks.cluster_name
+module "eks_blueprints_kubernetes_addons" {
+  source = "/home/ariel/terraform-aws-eks-blueprints/modules/kubernetes-addons"
 
-  irsa_oidc_provider_arn          = module.eks.oidc_provider_arn
-  irsa_namespace_service_accounts = ["karpenter:karpenter"]
+  eks_cluster_id       = module.eks.cluster_name
+  eks_cluster_endpoint = module.eks.cluster_endpoint
+  eks_oidc_provider    = module.eks.oidc_provider
+  eks_cluster_version  = module.eks.cluster_version
 
-  create_iam_role = false
-  iam_role_arn    = module.eks.eks_managed_node_groups["initial"].iam_role_arn
-
-  tags = {
-    Environment = "dev"
-    Terraform   = "true"
+  enable_argocd = true
+  # This example shows how to set default ArgoCD Admin Password using SecretsManager with Helm Chart set_sensitive values.
+  argocd_helm_config = {
+    set_sensitive = [
+      {
+        name  = "configs.secret.argocdServerAdminPassword"
+        value = bcrypt_hash.argo.id
+      }
+    ]
   }
+
+  keda_helm_config = {
+    values = [
+      {
+        name  = "serviceAccount.create"
+        value = "false"
+      }
+    ]
+  }
+
+  argocd_manage_add_ons = true # Indicates that ArgoCD is responsible for managing/deploying add-ons
+#  argocd_applications = {
+#    addons = {
+#      path               = "chart"
+#      repo_url           = "https://github.com/aws-samples/eks-blueprints-add-ons.git"
+#      add_on_application = true
+#    }
+#    workloads = {
+#      path = "."
+#      repo_url = "https://github.com/ArielDomchik/GitOps-dev.git"
+#    }
+#  }
+
+  # Add-ons
+  enable_amazon_eks_aws_ebs_csi_driver = true
+  enable_aws_for_fluentbit             = true
+  # Let fluentbit create the cw log group
+  aws_for_fluentbit_create_cw_log_group = false
+  enable_cert_manager                   = true
+  enable_cluster_autoscaler             = true
+  enable_karpenter                      = true
+  enable_keda                           = true
+  enable_metrics_server                 = true
+#  enable_prometheus                     = true
+  enable_traefik                        = true
+  enable_vpa                            = true
+  enable_yunikorn                       = true
+  enable_argo_rollouts                  = true
+
+  tags = local.tags
+}
+
+#---------------------------------------------------------------
+# ArgoCD Admin Password credentials with Secrets Manager
+# Login to AWS Secrets manager with the same role as Terraform to extract the ArgoCD admin password with the secret name as "argocd"
+#---------------------------------------------------------------
+resource "random_password" "argocd" {
+  length           = 16
+  special          = true
+  override_special = "!#$%&*()-_=+[]{}<>:?"
+}
+
+# Argo requires the password to be bcrypt, we use custom provider of bcrypt,
+# as the default bcrypt function generates diff for each terraform plan
+resource "bcrypt_hash" "argo" {
+  cleartext = random_password.argocd.result
+}
+
+#tfsec:ignore:aws-ssm-secret-use-customer-key
+resource "aws_secretsmanager_secret" "argocd" {
+  name                    = "argocd-secret"
+  recovery_window_in_days = 0 # Set to zero for this example to force delete during Terraform destroy
+}
+
+resource "aws_secretsmanager_secret_version" "argocd" {
+  secret_id     = aws_secretsmanager_secret.argocd.id
+  secret_string = random_password.argocd.result
 }
 
 
+
+
+################################################################################
+# Supporting Resources
+################################################################################
+
+module "vpc" {
+  source  = "terraform-aws-modules/vpc/aws"
+  version = "~> 4.0"
+
+  name = local.name
+  cidr = local.vpc_cidr
+
+  azs             = local.azs
+  private_subnets = [for k, v in local.azs : cidrsubnet(local.vpc_cidr, 4, k)]
+  public_subnets  = [for k, v in local.azs : cidrsubnet(local.vpc_cidr, 8, k + 48)]
+
+  enable_nat_gateway = true
+  single_nat_gateway = true
+
+  public_subnet_tags = {
+    "kubernetes.io/role/elb" = 1
+  }
+
+  private_subnet_tags = {
+    "kubernetes.io/role/internal-elb" = 1
+  }
+
+  tags = local.tags
+}

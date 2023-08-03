6c5acffa108845c88024b4cfff5b08798db48716
ariel.domchik <ariyonzor@gmail.com>
2023-08-03 16:28:44 +0300

    Test the selenium webdriver

--- src/seleniumunit.py
+++ src/seleniumunit.py
@@ -15,8 +15,9 @@ class TestSearch(unittest.TestCase):
         options.add_argument('--headless')
         options.add_argument('--no-sandbox')
         options.add_argument('--disable-dev-shm-usage')
-        s = Service('/home/ubuntu/chromedriver.exe')
-        self.driver = webdriver.Chrome(service=s, options=options)
+        driver = webdriver.Chrome()
+#        s = Service('/home/ubuntu/chromedriver.exe')
+#        self.driver = webdriver.Chrome(service=s, options=options)
 
 
 
